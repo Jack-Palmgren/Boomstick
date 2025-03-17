81b0c4b85dde8a08f348f6b7565df236f59cbd0e
Jack-Palmgren <jpalmgren@umassd.edu>
2025-03-17 01:43:52 -0400

    Beta version of the game done. This includes animations, GUI, sounds, ect.

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private GameObject _player;
+    [SerializeField] private GameObject textBox;
+    public string textBoxText = "";
+
+    Vector3 camPos = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textBox.GetComponent<TextMeshProUGUI>().text = textBoxText;
+        camPos = _player.transform.position;
+        camPos.z = -10;
+        _camera.transform.position = camPos;
+    }
+}
---
+++ Assets/slashTester.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D.Animation;
+
+public class slashTester : MonoBehaviour
+{
+    public float speed = 5f;
+    public float maxValFS = 10f;
+    private float counter = 4f;
+    private float frameSkip; //(MaxValFrameskip): Every 10 frames the animation switches to the next image
+    private SpriteResolver spriteResolver;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        spriteResolver = GetComponent<SpriteResolver>();
+        rb = GetComponent<Rigidbody2D>();
+        frameSkip = maxValFS/8;
+        //Debug.Log(frameSkip);
+    }
+
+    void Update()
+    {
+        if (frameSkip <= 0f)
+        {
+            ChangeSprite("Slash", counter);
+            counter++;
+            if (counter > 8)
+            {
+                counter = 0;
+                Destroy(gameObject); //Destroy it after the animation plays
+            }
+            frameSkip = maxValFS/8;
+        }
+        frameSkip--;
+    }
+
+    void ChangeSprite(string category, float label)
+    {
+        if (spriteResolver)
+        {
+            spriteResolver.SetCategoryAndLabel(category, label.ToString()); // Change dynamically
+        }
+    }
+}
